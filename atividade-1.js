b77036b7abdf6071f90834a3f4ca32a06333fc44
mavi-ot <v.maria@estudante.ifro.edu.br>
2025-04-03 11:30:35 -0400

    atividade1-03/04

---
+++ atividade-1.js
@@ -0,0 +1,26 @@
+//1
+let nome = "Maria Vitória";
+let idade = "15";
+console.log('Olá, meu nome é',nome, 'e eu tenho' ,idade,"anos");
+
+//2
+let string = "Julia Vitória"
+lower = string.toLowerCase()
+console.log(lower)
+upper = string.toUpperCase()
+console.log(upper)
+
+//3
+let num = "15"
+let int = 15
+
+let text = (typeof num)
+console.log(text)
+let numero = (typeof int)
+console.log(numero)
+
+//4
+let peso = 44
+let altura = 1.54
+let imc = peso / (altura * altura)
+console.log(imc)
