bee27cee6106479de014f24f4d498723ffcdf7ae
mavi-ot <v.maria@estudante.ifro.edu.br>
2025-04-10 11:39:48 -0400

    atividade 02

---
+++ atividade02.js
@@ -0,0 +1,33 @@
+//1
+let num1 = 19
+let num2 = 25
+
+let soma = (num1 + num2);
+console.log("Soma:" +soma);
+
+let sub = (num1 - num2);
+console.log("Subtração:" +sub);
+
+let mult = (num1 * num2);
+console.log("Multiplicação:" +mult);
+
+let div = (num1 / num2);
+console.log("Divisão:" +div);
+
+
+//2
+let idade1 = 15
+let idade2 = 17
+
+let resultado = idade1 == idade2
+console.log(resultado);
+
+//3
+let idade = 20;
+let mensagem = idade>= 18? "Maior de idade" : "Menor de idade";
+console.log(mensagem);
+
+//4
+let numero = 15;
+let verificação = (numero % 2 === 0) ? "par" : "ímpar";
+console.log(`O numero ${numero} é ${verificação}`);
